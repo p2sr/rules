5a3be2259a1a258fb4b64129fb20e5a19c2b19d7
Soni <soni@recalstudios.net>
2024-09-03 20:31:30 +0200

    fix: scroll to top of nav menu when it closes

--- out/mobile-menu.js
+++ out/mobile-menu.js
@@ -1,5 +1,6 @@
 document.querySelectorAll("nav img, nav a").forEach(e => {
     e.addEventListener("click", () => {
         document.querySelector("nav").classList.toggle("visible");
+        document.querySelector("nav").scrollTo({ top: 0, behavior: "smooth" }); // Reset scroll position
     })
 });
